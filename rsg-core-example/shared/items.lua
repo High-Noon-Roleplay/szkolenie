627163a49c124f74f065df67a610d81eed027526
Andmarski <andmar553@gmail.com>
2025-09-27 18:27:20 +0200

    Format item definitions for better readability in items.lua

--- rsg-core-example/shared/items.lua
+++ rsg-core-example/shared/items.lua
@@ -1,9 +1,9 @@
 RSGShared = RSGShared or {}
 RSGShared.Items = {
 
-    bread      = { label = 'Bread',         weight = 100},
-    water      = { label = 'Water',         weight = 100},
-    stew       = { label = 'Stew',          weight = 100},
-    coffee     = { label = 'Coffee',        weight = 100},
+    bread  = { label = 'Bread',  weight = 100},
+    water  = { label = 'Water',  weight = 100},
+    stew   = { label = 'Stew',   weight = 100},
+    coffee = { label = 'Coffee', weight = 100}
 
 }
