9e37517069e3cad3b072f49ba4dd777ee1babaca
Zatryk <zatryk.123@gmail.com>
2025-09-27 21:16:58 +0200

    gdgdjk

--- rsg-core-example/shared/items.lua
+++ rsg-core-example/shared/items.lua
@@ -2,9 +2,9 @@ RSGShared = RSGShared or {}
 RSGShared.Items = {
 
     bread      = { label = 'Chleb do jedzenia',       weight = 100},
-    water      = { label = 'Woda',        weight = 100},
-    stew       = { label = 'Gulasz',      weight = 100},
-    coffee     = { label = 'Kawa',        weight = 100},
+    water      = { label = 'hfhWoda',        weight = 100},
+    stew       = { label = 'Gulhfhasz',      weight = 100},
+    coffee     = { label = 'Kafhfwa',        weight = 100},
 
 
 }
