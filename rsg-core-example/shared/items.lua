9dfcb3b39e1b3625b3091efcb1a0c8407b95f22f
Zatryk <zatryk.123@gmail.com>
2025-09-27 21:13:50 +0200

    chleb

--- rsg-core-example/shared/items.lua
+++ rsg-core-example/shared/items.lua
@@ -1,10 +1,10 @@
 RSGShared = RSGShared or {}
 RSGShared.Items = {
 
-    bread      = { label = 'Chleb',       weight = 12300},
-    water      = { label = 'Woda',        weight = 1050},
-    stew       = { label = 'Gulasz',      weight = 10430},
-    coffee     = { label = 'Kawa',        weight = 1060},
+    bread      = { label = 'Chleb do jedzenia',       weight = 100},
+    water      = { label = 'Woda',        weight = 100},
+    stew       = { label = 'Gulasz',      weight = 100},
+    coffee     = { label = 'Kawa',        weight = 100},
 
 
 }
