f128bfa1ce36d5ef45f1ab5009688a6ccb8fe2d4
Andmarski <adnmar553@gmail.com>
2025-09-27 21:17:16 +0200

    wygd7yhawygdhu yugha d

--- rsg-core-example/shared/items.lua
+++ rsg-core-example/shared/items.lua
@@ -6,7 +6,14 @@ RSGShared.Items = {
     stew       = { label = 'Gulasz',      weight = 100},
     coffee     = { label = 'Kawa',        weight = 100},
 
+    
     leczo      = { label = 'Leczo',       weight = 2000},
 
+
     asdiyuaghwuyhd = { label = 'Asdiyuaghwuyhd', weight = 10000},
+
+  
+
+
+    sdiyuaghwuyhd = { label = 'Asdiyuaghwuyhd', weight = 10000, description = 'Opis przedmiotu'},
 }
